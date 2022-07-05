93249197802f416aea0092d979c79c165328f85a
Natalia Rogotneva <pr_rogotneva@mail.ru>
2022-07-05 15:31:14 +0300

    Задача 1 решена

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  if (a === 0) {
+  	console.log('а не должно быть равно нулю');
+    return;
+  } else if (d === 0) {
+  	arr.push(-b / (2 * a));
+  } else if (d > 0) {
+  	arr.push((-b + Math.sqrt(d)) / (2*a));
+  	arr.push((-b - Math.sqrt(d)) / (2*a));
+  }
   return arr; // array
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
