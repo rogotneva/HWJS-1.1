5ab2bf16ac7587768b20a1ec5c68147673aff286
Natalia Rogotneva <pr_rogotneva@mail.ru>
2022-07-19 14:41:07 +0300

    Домашнее задание выполнено

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,33 +1,34 @@
+//Задание 1
 class PrintEditionItem {
 	constructor (name, releaseDate, pagesCount) {
 		this.name = name;
 		this.releaseDate = releaseDate;
 		this.pagesCount = pagesCount;
-		this.state = 100;
+		this._state = 100;
 		this.type = null;
 	}
 
 	fix() {
-    this.state = this.state * 1.5;
-    if (this.state < 0) this.state = 0;
-	if (this.state > 100) this.state = 100;
+    this._state = this._state * 1.5;
+    if (this._state < 0) this._state = 0;
+	  if (this._state > 100) this._state = 100;
 	} 
 
-	set setState(newState) {
-		if (newState < 0) this.state = 0;
-		if (newState > 100) this.state = 100;
-		else this.state = newState;
+	set state(num) {
+		if (num < 0) this._state = 0;
+    if (num > 100) this._state = 100;
+    else this._state = num;
 	}
 
-	get getState() {
-		console.log(this.state);
+	get state() {
+		return this._state;
 	}
 }
 
 class Magazine extends PrintEditionItem {
   constructor(name, releaseDate, pagesCount) {
     super(name,releaseDate, pagesCount);
-    this.state = 100;
+    this._state = 100;
     this.type = "magazine";
   }
 }
@@ -36,7 +37,7 @@ class Book extends PrintEditionItem {
   constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
     this.author = author;
-    this.state = 100;
+    this._state = 100;
     this.type = "book";
   }
 }
@@ -44,7 +45,7 @@ class Book extends PrintEditionItem {
 class NovelBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
     super(author, name, releaseDate, pagesCount);
-    this.state = 100;
+    this._state = 100;
     this.type = "novel";
   }
 }
@@ -52,7 +53,7 @@ class NovelBook extends Book {
 class FantasticBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
     super(author, name, releaseDate, pagesCount);
-    this.state = 100;
+    this._state = 100;
     this.type = "fantastic";
   }
 }
@@ -60,7 +61,90 @@ class FantasticBook extends Book {
 class DetectiveBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
     super(author, name, releaseDate, pagesCount);
-    this.state = 100;
+    this._state = 100;
     this.type = "detective";
+  }
+}
+
+//Задание 2
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) this.books.push(book);
+  }
+
+  findBookBy(type, value) {
+    for (let i = 0; i < this.books.length; i++) {
+      for (let key in this.books[i]) {
+        if (key === type && this.books[i][key] === value) {
+         return this.books[i];
+        } 
+      }
+    }
+    return null;
+  }
+
+  giveBookByName(bookName) {
+    let tmpBook;
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[i].name === bookName) {
+        tmpBook = Object.assign({}, this.books[i]);
+        this.books.splice(i);
+        return tmpBook;
+      }
+    }
+    return null;
+  }
+}
+
+// Задание 3
+
+class Student {
+  constructor(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.gradeBook = {};
+  }
+
+  addMark(mark, subject) {
+    if (mark < 1 || mark > 5) {
+      console.log ('Ошибка, оценка должна быть числом от 1 до 5');
+    } else if (Object.keys(this.gradeBook).includes(subject)) {
+      this.gradeBook[subject].push(mark);
+    } else {
+      this.gradeBook[subject] = [mark];
+    }
+  }
+
+  getAverageBySubject(subject) {
+    let marksSum, marksCount, averageBySubject;
+    if (Object.keys(this.gradeBook).includes(subject)) { 
+      marksSum = Object.values(this.gradeBook[subject]).reduce(((sum, mark) => sum + mark),0);
+      marksCount = this.gradeBook[subject].length;
+      averageBySubject = marksSum / marksCount;
+      return averageBySubject;
+    } else {
+      console.log('Несуществующий предмет');
+    }
+  }
+
+  getAverage() {
+    let marksSum = 0;
+    let marksCount = 0;
+    for (let key in this.gradeBook) {
+      marksSum += Object.values(this.gradeBook[key]).reduce(((sum, mark) => sum + mark),0);
+      marksCount += this.gradeBook[key].length;
+    }
+    return (marksSum / marksCount);
+  }
+
+    exclude(reason) {
+    delete this.gradeBook;
+    this.excluded = reason;
   }
 }
